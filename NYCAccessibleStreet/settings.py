065a0e5c38e8325ad9bd1cd746a37bed9a95debe
Sarthak Gagdani <sg6367@nyu.edu>
2022-10-04 17:49:40 -0400

    added gitignore and organized directory

--- NYCAccessibleStreet/settings.py
+++ NYCAccessibleStreet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ')%!lxtf=@r46#rcv-dcqy-(smx7n=bm2@lsn1#jkh(e+*(_mhe'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
