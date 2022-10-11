23636b9642347c5ff0d24c613066915efb1813e4
Sarthak Gagdani <31386718+SarthakGagdani@users.noreply.github.com>
2022-10-11 12:50:39 -0400

    added map component to the home page (#37)
    * added landing_map app
    
    * Integrated map component and filter section

--- NYCAccessibleStreet/urls.py
+++ NYCAccessibleStreet/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('mainmap/', include('mainmap.urls')),
     path('admin/', admin.site.urls),
+    path('', include('landing_map.urls')),
 ]
---
+++ landing_map/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from decouple import config
+
+def index(request):
+    context = {
+       "mapboxAccessToken": config('MAPBOX_PUBLIC_TOKEN')
+    }
+    return render(request, 'landing_map/home.html', context)
