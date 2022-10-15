3f7e2ef3347077bba84eb599afce9c713a840a20
ab9649 <ames.bartels@gmail.com>
2022-10-15 13:59:38 -0400

    Linked app to DB on RDS Initialized app for reporting

--- NYCAccessibleStreet/settings.py
+++ NYCAccessibleStreet/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing_map.apps.LandingMapConfig'
+    'landing_map.apps.LandingMapConfig',
+    'report.apps.ReportConfig'
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'NYCAccessibleStreet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('dbname'),
+        'USER': config('dbuser'),
+        'PASSWORD': config('dbpassword'),
+        'HOST': config('dbhost'),
+        'PORT': '3306',
     }
 }
 
