92ad16139db34c7d0a828aab63283a8900b5121d
ab9649 <ames.bartels@gmail.com>
2022-10-15 13:59:38 -0400

    Linked app to DB on RDS Initialized app for reporting

--- landing_map/admin.py
+++ landing_map/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Favorite
+from .models import Infra_type
+from .models import Accessible_location
 
 # Register your models here.
+
+admin.site.register(Infra_type)
+admin.site.register(Favorite)
+admin.site.register(Accessible_location)
