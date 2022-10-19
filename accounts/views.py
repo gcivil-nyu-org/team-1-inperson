3b12043b30e310aaf11610324541026babb94ebc
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-10-19 16:59:49 -0400

    login/signup pages and fixing navbar display

--- accounts/views.py
+++ accounts/views.py
@@ -6,4 +6,7 @@ from django.views import generic
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+    template_name = "accounts/registration/signup.html"
+
+# class loginview(generic.CreateView):
+#     template_name="accounts/registration/login.html"
