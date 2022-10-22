6a4f7ae05d99c54b7bfd09dcb39299ba1f047630
Simone B <snb331@nyu.edu>
2022-10-22 01:33:49 -0400

    URL unit tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTests(TestCase):
+
+    def  test_signup_page(self):
+        response = self.client.get("signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        response = self.client.get("login/")
+        self.assertEqual(response.status_code, 200)
+
