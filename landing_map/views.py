1a5ce7ad3cfe9086969273e165677da925725be9
Sarthak Gagdani <sg6367@nyu.edu>
2022-10-22 21:09:31 -0400

    Reformatted the code

--- landing_map/views.py
+++ landing_map/views.py
@@ -3,10 +3,13 @@ from decouple import config
 from .models import Infra_type, Favorite, Accessible_location
 from django.core import serializers
 
+
 def index(request):
-    accessible_locations = serializers.serialize("json", Accessible_location.objects.all())
+    accessible_locations = serializers.serialize(
+        "json", Accessible_location.objects.all()
+    )
     context = {
         "mapboxAccessToken": config("MAPBOX_PUBLIC_TOKEN"),
-        "accessible_locations": accessible_locations
+        "accessible_locations": accessible_locations,
     }
     return render(request, "landing_map/home.html", context)
