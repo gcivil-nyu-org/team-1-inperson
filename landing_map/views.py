fcea392dd5ff005698212b4f0279e04444cecaee
Sarthak Gagdani <sg6367@nyu.edu>
2022-10-22 20:57:19 -0400

    1. Added accessible infra markers on map 2. Added popup on pins

--- landing_map/views.py
+++ landing_map/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from decouple import config
-
+from .models import Infra_type, Favorite, Accessible_location
+from django.core import serializers
 
 def index(request):
-    context = {"mapboxAccessToken": config("MAPBOX_PUBLIC_TOKEN")}
+    accessible_locations = serializers.serialize("json", Accessible_location.objects.all())
+    context = {
+        "mapboxAccessToken": config("MAPBOX_PUBLIC_TOKEN"),
+        "accessible_locations": accessible_locations
+    }
     return render(request, "landing_map/home.html", context)
