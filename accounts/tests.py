40977999428182be92031e2d620221c2c29dbb28
Simone B <snb331@nyu.edu>
2022-10-23 20:41:05 -0400

    added user tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,60 +1,135 @@
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth import login, authenticate
 from .forms import CreateUserForm
+
 
 # Create your tests here.
 
 client = Client()
+
+
 class URLTests(TestCase):
-    def  test_signup_page(self):
+    def test_signup_page(self):
         response = client.get("/accounts/signup/")
         self.assertEqual(response.status_code, 200)
-
 
     def test_login_page(self):
         response = client.get("/accounts/login/")
         self.assertEqual(response.status_code, 200)
 
-class RegisterTest(TestCase):
-    username = 'testy'
-    email = 'testy@fake.com'
-    first_name = 'Test'
-    last_name = 'User'
-    password1 = 'password'
-    bad_password2 = 'notpassword'
-    def test_user_added_to_database(self):
-        response = client.post("/accounts/login/")
-        print(response)
-        self.assertEqual(4,4)
 
-    def test_passwords_match(self):
-        # TODO
-        pass
+class RegisterTest(TestCase):
+    def test_user_added_to_database(self):
+        post = {
+            "username": "realuser",
+            "email": "someone@domain.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "password1": "something_very_s3cur3",
+            "password2": "something_very_s3cur3",
+        }
+        form = CreateUserForm(post)
+        form.save()
 
     def test_passwords_dont_match(self):
-        # TODO
-        pass
+        post = {
+            "username": "realuser",
+            "email": "someone@domain.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "password1": "something_very_s3cur3",
+            "password2": "something_else",
+        }
+        form = CreateUserForm(post)
+        try:
+            form.save()
+        except ValueError:
+            self.assertTrue(True)
+            return
+        raise Exception("system accepted mismatched passwords")
+
+    def test_insecure_password_fails(self):
+        post = {
+            "username": "realuser",
+            "email": "someone@domain.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "password1": "password",
+            "password2": "password",
+        }
+        form = CreateUserForm(post)
+        try:
+            form.save()
+        except ValueError:
+            self.assertTrue(True)
+            return
+        raise Exception("system accepted insecure password")
+
 
 class LoginTest(TestCase):
-    good_username = 'realuser'
-    good_password = 'password'
-    bad_unsername = 'baduser'
-    bad_password = 'notpassword'
-    # TODO: add user to database to test
-
-
     def test_real_user_authenticates(self):
-        # TODO
-        pass
+        post = {
+            "username": "realuser",
+            "email": "someone@domain.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "password1": "something_very_s3cur3",
+            "password2": "something_very_s3cur3",
+        }
+        form = CreateUserForm(post)
+        form.save()
+        request = client.post("/accounts/login/")
+        user = authenticate(
+            request, username="realuser", password="something_very_s3cur3"
+        )
+        messy_fix = user
+        messy_fix += "used"  # flake8 doesn't recognize that eval uses user
+        self.assertTrue(eval("user != None"))
 
     def test_valid_username_invalid_password(self):
-        # TODO
-        pass
+        post = {
+            "username": "realuser2",
+            "email": "someoneelse@domain.com",
+            "first_name": "Test2",
+            "last_name": "User",
+            "password1": "something_very_s3cur3",
+            "password2": "something_very_s3cur3",
+        }
+        form = CreateUserForm(post)
+        form.save()
+        request = client.post("/accounts/login/")
+        user = authenticate(request, username="realuser", password="wrong_password")
+        self.assertEqual(user, None)
 
     def test_invalid_username_valid_password(self):
-        # TODO
-        pass
+        post = {
+            "username": "realuser3",
+            "email": "someoneelse@domain.com",
+            "first_name": "Test2",
+            "last_name": "User",
+            "password1": "something_very_s3cur3",
+            "password2": "something_very_s3cur3",
+        }
+        form = CreateUserForm(post)
+        form.save()
+        request = client.post("/accounts/login/")
+        user = authenticate(
+            request, username="realuser", password="something_very_s3cur3"
+        )
+        self.assertEqual(user, None)
 
     def test_invalid_username_invalid_password(self):
-        # TODO
-        pass
+        post = {
+            "username": "realuser3",
+            "email": "someoneelse@domain.com",
+            "first_name": "Test2",
+            "last_name": "User",
+            "password1": "something_very_s3cur3",
+            "password2": "something_very_s3cur3",
+        }
+        form = CreateUserForm(post)
+        form.save()
+        request = client.post("/accounts/login/")
+        user = authenticate(request, username="realuser", password="wrong_password")
+        self.assertEqual(user, None)
