80c2df56ee4d38868587c955158e056176a9c85f
Sarthak Gagdani <31386718+SarthakGagdani@users.noreply.github.com>
2022-10-24 14:59:54 -0400

    Issue13 sarthak accessible infra pins (#66)
    * 1. Added accessible infra markers on map
    2. Added popup on pins
    
    * Reformatted the code

--- static/landing_map/maps.js
+++ static/landing_map/maps.js
@@ -6,10 +6,40 @@ const map = new mapboxgl.Map({
     zoom: 11
 });
 
+const infra_types_url = {1:"ramp", 2:"pole", 3:"sidewalk"};
+const infra_types = {1:"Accessible Pedestrian Ramps", 2:"Accessible Pedestrian Signals", 3: "Accessible Pedestrian Sidewalks"};
+
+accessible_locations.forEach(function (accessible_location) {
+
+    const el = document.createElement('div');
+    const isAccessible = accessible_location.fields.isAccessible
+    const locX = accessible_location.fields.locationX
+    const locY = accessible_location.fields.locationY
+    const infraType = accessible_location.fields.typeID
+    const isAccessibleString = isAccessible ? "": "Not"
+    const pin_url = `/static/img/pins/${infra_types_url[infraType]}-${isAccessible?'a':'na'}.png`
+
+    el.className = 'marker';
+    el.style.backgroundImage = `url(${pin_url})`;
+    el.style.width = `40px`;
+    el.style.height = `40px`;
+    el.style.backgroundSize = '100%';
+
+    const popupMessage = `<strong> ${infra_types[infraType]} </strong>
+                          <p>This is currently <strong> ${isAccessibleString} </strong> Accessible </p>`
+
+    var marker = new mapboxgl.Marker(el)
+        .setLngLat([locX, locY])
+        .setPopup(new mapboxgl.Popup().setHTML(popupMessage))
+        .addTo(map);
+});
+
+
 // Add the control to the map.
 const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
+    mapboxgl: mapboxgl,
+    zoom: 14
 });
 
 geocoder.on('result', (e) => {
