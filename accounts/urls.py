8c5fd97d2882178a9a6d446d71ab930c89e64fc4
ab9649 <ames.bartels@gmail.com>
2022-10-29 22:36:14 -0400

    verify email address during signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import activate
 
 
 urlpatterns = [
     path("signup/", views.register_page, name="signup"),
     path("login/", views.login_page, name="login"),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', activate, name='activate'),
+      
 ]
