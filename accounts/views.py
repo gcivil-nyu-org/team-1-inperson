7e47d45ea88fdbef0807fbc97fd00c04b1881389
ab9649 <ames.bartels@gmail.com>
2022-10-30 18:06:13 -0400

    missed import InputForm() for help page

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .tokens import account_activation_token
 from django.contrib.auth.models import User
+from .forms import InputForm
 
 
 def register_page(request):
