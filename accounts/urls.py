fe03234e112079fa1aaaf1d02015ff7f4ab271cc
ab9649 <ames.bartels@gmail.com>
2022-10-29 22:59:27 -0400

    flake8

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("signup/", views.register_page, name="signup"),
     path("login/", views.login_page, name="login"),
     path(
-        "activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",
+        r"activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",
         activate,
         name="activate",
     ),
