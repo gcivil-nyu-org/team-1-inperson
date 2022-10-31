881a803428be2901a944fb3ea0aacc7236d52e10
Sarthak Gagdani <31386718+SarthakGagdani@users.noreply.github.com>
2022-10-31 16:05:57 -0400

    Issue63 sarthak filter button (#95)
    * added radius range selector button in filter box
    
    * made filter button functional and added js functions to handle filter and query params
    
    * Applying filter params to the checkboxes after reloading
    
    * added geolocation api support and refactored issues in maps.js
    
    * added radius query to filter data nearby
    
    * reformatted code using black

--- landing_map/views.py
+++ landing_map/views.py
@@ -8,7 +8,7 @@ import requests
 def populate_cards():
     cardList = []
     mapbox_host = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
-    qset = Accessible_location.objects.filter()[:30]
+    qset = Accessible_location.objects.filter()[:1]
     card_id = 1
     address_list = []
 
@@ -95,10 +95,50 @@ def populate_cards():
 
 
 def index(request):
+
+    filterParams = request.GET
+
+    if filterParams.get("currentlyAccessible"):
+        currentlyAccessible = []
+        if filterParams.get("currentlyAccessible") == "true":
+            currentlyAccessible.append(True)
+        if filterParams.get("currentlyInaccessibleCheck") == "true":
+            currentlyAccessible.append(False)
+        else:
+            currentlyAccessible.append(True)
+
+        infraTypes = []
+        if filterParams.get("rampsCheck") == "true":
+            infraTypes.append("1")
+        if filterParams.get("poleCheck") == "true":
+            infraTypes.append("2")
+        if filterParams.get("sidewalkCheck") == "true":
+            infraTypes.append("3")
+
+        radiusQuery = (
+            "SELECT infraID, ( 3959 * acos( cos( radians({y}) ) * cos( radians(locationY) ) * cos( radians(locationX) - radians({x}) ) "
+            "+ sin( radians({y}) ) * sin(radians(locationY)) ) ) AS distance FROM landing_map_accessible_location HAVING distance < "
+            "{radius} ORDER BY distance".format(
+                y=filterParams.get("y-co"),
+                x=filterParams.get("x-co"),
+                radius=filterParams.get("radiusRange"),
+            )
+        )
+
+        infraIds = []
+        nearbyLocations = Accessible_location.objects.raw(radiusQuery)
+        for loc in nearbyLocations:
+            infraIds.append(loc.infraID)
+
+        filteredLocations = Accessible_location.objects.filter(
+            infraID__in=infraIds,
+            isAccessible__in=currentlyAccessible,
+            typeID__in=infraTypes,
+        )
+    else:
+        filteredLocations = Accessible_location.objects.all()
     cardList, address_list = populate_cards()
-    accessible_locations = serializers.serialize(
-        "json", Accessible_location.objects.all()
-    )
+    accessible_locations = serializers.serialize("json", filteredLocations)
 
     context = {
         "mapboxAccessToken": config("MAPBOX_PUBLIC_TOKEN"),
--- static/landing_map/maps.js
+++ static/landing_map/maps.js
@@ -1,51 +1,140 @@
-const map = new mapboxgl.Map({
-    container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74, 40.67],
-    zoom: 11
-});
+currentLocation = {};
+searchedLocation = {};
+var mapLocation =  [-73.98, 40.694];
+const mapboxHost = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+const mapboxParams = {"access_token": mapboxgl.accessToken, "types": "address"}
 
+filterParams =  Object.fromEntries(new URLSearchParams(document.URL.split('?')[1]));
+// Sample lat = 40. long = -74
 const infra_types_url = {1:"ramp", 2:"pole", 3:"sidewalk"};
-const infra_types = {1:"Accessible Pedestrian Ramps", 2:"Accessible Pedestrian Signals", 3: "Accessible Pedestrian Sidewalks"};
-
-accessible_locations.forEach(function (accessible_location) {
-
-    const el = document.createElement('div');
-    const isAccessible = accessible_location.fields.isAccessible
-    const locX = accessible_location.fields.locationX
-    const locY = accessible_location.fields.locationY
-    const infraType = accessible_location.fields.typeID
-    const isAccessibleString = isAccessible ? "": "Not"
-    const pin_url = `/static/img/pins/${infra_types_url[infraType]}-${isAccessible?'a':'na'}.png`
-
-    el.className = 'marker';
-    el.style.backgroundImage = `url(${pin_url})`;
-    el.style.width = `40px`;
-    el.style.height = `40px`;
-    el.style.backgroundSize = '100%';
-
-    const popupMessage = `<strong> ${infra_types[infraType]} </strong>
-                          <p>This is currently <strong> ${isAccessibleString} </strong> Accessible </p>`
-
-    var marker = new mapboxgl.Marker(el)
-        .setLngLat([locX, locY])
-        .setPopup(new mapboxgl.Popup().setHTML(popupMessage))
-        .addTo(map);
-});
+const infra_types = {1:"Accessible Pedestrian Ramps", 2:"Accessible Pedestrian Signals", 3: "Raised Pedestrian Sidewalks"};
 
 
-// Add the control to the map.
-const geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl,
-    zoom: 14
-});
+if(Object.keys(filterParams).length != 0){
+    mapLocation = [ JSON.parse(filterParams['x-co']), JSON.parse(filterParams['y-co'])]
+    searchedLocation['longitude'] = JSON.parse(filterParams['x-co']);
+    searchedLocation['latitude'] = JSON.parse(filterParams['y-co']);
+    plotMap()
+}
+else {
+    getLocation();
+}
 
-geocoder.on('result', (e) => {
-    console.log(JSON.stringify(e.result, null, 2))
-});
+async function fetchAsync(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
 
-document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-map.addControl(new mapboxgl.FullscreenControl());
-map.addControl(new mapboxgl.NavigationControl());
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, positionError);
+
+  } else {
+    console.log("Geolocation is not supported by this device")
+
+  }
+}
+
+function showPosition(position) {
+    currentLocation['longitude'] = position.coords.longitude;
+    currentLocation['latitude'] = position.coords.latitude;
+    console.log("currentLocation", currentLocation)
+    mapLocation = [position.coords.longitude, position.coords.latitude]
+    redirect_to_url();
+}
+
+function positionError() {
+    alert('Geolocation is not enabled. Please enable to use this feature')
+    currentLocation['longitude'] = mapLocation[0];
+    currentLocation['latitude'] = mapLocation[1];
+    redirect_to_url();
+}
+
+function plotMap(){
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: mapLocation,
+        zoom: 11
+    });
+
+    map.on('load', function () {
+        map.flyTo({
+           center: mapLocation,
+           essential: true,
+           zoom: 14
+        });
+    });
+
+    const mapboxURL = mapboxHost + mapLocation[0] + "," + mapLocation[1] + ".json?" + new URLSearchParams(mapboxParams)
+
+    fetchAsync(url=mapboxURL).then(response=>{
+
+        const el1 = document.createElement('div1');
+        el1.style.backgroundImage = `url('/static/img/pins/pin.png')`;
+        el1.className = 'marker';
+        el1.style.width = `40px`;
+        el1.style.height = `40px`;
+        el1.style.backgroundSize = '100%';
+        let popupMessage = `<br/><strong>${response["features"][0]["place_name"]}<strong>`
+
+        var marker = new mapboxgl.Marker(el1)
+            .setLngLat(mapLocation)
+            .setPopup(new mapboxgl.Popup().setHTML(popupMessage))
+            .addTo(map);
+    });
+
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+            enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
+        })
+    );
+
+    accessible_locations.forEach(function (accessible_location) {
+
+        const isAccessible = accessible_location.fields.isAccessible
+        const locX = accessible_location.fields.locationX
+        const locY = accessible_location.fields.locationY
+        const infraType = accessible_location.fields.typeID
+        const isAccessibleString = isAccessible ? "": "Not"
+        const pin_url = `/static/img/pins/${infra_types_url[infraType]}-${isAccessible?'a':'na'}.png`
+
+        const el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = `url(${pin_url})`;
+        el.style.width = `40px`;
+        el.style.height = `40px`;
+        el.style.backgroundSize = '100%';
+
+        const popupMessage = `<br/><strong> ${infra_types[infraType]} </strong>
+                              <p>This is currently <strong> ${isAccessibleString} </strong> Accessible </p>`
+
+        var marker = new mapboxgl.Marker(el)
+            .setLngLat([locX, locY])
+            .setPopup(new mapboxgl.Popup().setHTML(popupMessage))
+            .addTo(map);
+    });
+
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        flyTo: false,
+        marker: false
+    });
+
+    geocoder.on('result', (e) => {
+        searchedLocation['longitude'] = e.result.geometry.coordinates[0];
+        searchedLocation['latitude'] = e.result.geometry.coordinates[1];
+        console.log("searchedLocation", searchedLocation)
+        redirect_to_url();
+    });
+
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+    map.addControl(new mapboxgl.FullscreenControl());
+    map.addControl(new mapboxgl.NavigationControl());
+}
