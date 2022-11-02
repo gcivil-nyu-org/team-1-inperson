9b6c5246f3b92bbc78e6a3f93b50182f919f3fb9
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-11-02 12:35:18 -0400

    bootstrap 4 navbar on login/signup (#96)
    * bootstrap 4 navbar on login/signup
    
    * url path updates to include landing page
    
    * url path updates to include landing page
    
    * url path updates to include landing page
    
    * black formatting
    
    * search link to home page
    
    * images
    
    * fixed merge conflicts and failed test cases
    
    * Fixed navbar route
    
    Co-authored-by: Sarthak Gagdani <sg6367@nyu.edu>

--- NYCAccessibleStreet/urls.py
+++ NYCAccessibleStreet/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("landing_map.urls")),
+    path("home/", include("landing_map.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
