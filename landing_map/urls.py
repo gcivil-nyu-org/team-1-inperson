484789bb248d8aa8785dab4a3ca478b032bcacb7
Sarthak Gagdani <sg6367@nyu.edu>
2022-11-07 20:06:10 -0500

    reformatted with black

--- landing_map/urls.py
+++ landing_map/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.landingpage, name="landingpage"),
     path("home/", views.index, name="home"),
-    path("report", views.report, name = "report"),
-    path("resolve_report", views.resolve_report, name = "resolve_report"),
+    path("report", views.report, name="report"),
+    path("resolve_report", views.resolve_report, name="resolve_report"),
     path("myFav/", views.myFav, name="myFav"),
 ]
