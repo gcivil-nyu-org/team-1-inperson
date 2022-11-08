67059824f2d3fda64d158677f8ebfc6b71eacaac
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-11-08 15:48:57 -0500

    test case for nycaccessiblestreets

--- NYCAccessibleStreet/tests.py
+++ NYCAccessibleStreet/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-from .utils import populate_cards_by_address
+from .utils import populate_cards_by_address, test_dict
 
 
 class NYCAccessibleStreetsTests(TestCase):
     def populate_cards_test(self):
+        test_dict()
         populate_cards_by_address()
