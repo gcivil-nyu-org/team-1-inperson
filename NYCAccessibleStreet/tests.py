d115fa304c9e96e5207cd30a55f4822f6d898d1c
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-11-08 23:29:29 -0500

    restructure test files

--- NYCAccessibleStreet/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .utils import populate_cards_by_address, test_dict
-
-
-class NYCAccessibleStreetsTests(TestCase):
-    def populate_cards_test(self):
-        test_dict()
-        populate_cards_by_address()
