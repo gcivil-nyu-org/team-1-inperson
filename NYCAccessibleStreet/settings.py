2423ae4d25835a9ff05b74b2908b19189aa971ea
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-11-13 15:37:02 -0500

    404 error template

--- NYCAccessibleStreet/settings.py
+++ NYCAccessibleStreet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = [
     "localhost",
